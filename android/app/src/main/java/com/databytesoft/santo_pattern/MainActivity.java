433ffa28dda36b5e26258bad0a08bf2f0cefba81
Santo Shakil <shakil.mezbah@gmail.com>
2021-12-19 16:10:56 +0600

    upgraded to flutter 2.8

--- android/app/src/main/java/com/databytesoft/santo_pattern/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.databytesoft.pattern_s;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/example/pattern_s/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pattern_s
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
